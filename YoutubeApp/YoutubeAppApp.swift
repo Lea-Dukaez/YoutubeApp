689f38d1f2888d652592aa536d4a1c6fdd55303d
Lea <lea.dukaez@gmail.com>
2021-10-10 15:37:21 +0200

    Initial Commit

---
+++ YoutubeApp/YoutubeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeAppApp.swift
+//  YoutubeApp
+//
+//  Created by Léa Dukaez on 10/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct YoutubeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
